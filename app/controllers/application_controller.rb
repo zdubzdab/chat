c5a3c250460c28a16f67ea5cdc46bbd193469015
zdubzdab <zdubzdab@gmail.com>
2017-06-07 11:36:02 +0300

    Restrict access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :unauthorized
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def unauthorized
+    redirect_to sessions_new_path, notice: 'You dont have permission to do '\
+      'this.' unless current_user
+  end
 end
