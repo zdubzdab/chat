5fac9ae9b39cdacaea9a30eeb7dd29eb8bac53d4
zdubzdab <zdubzdab@gmail.com>
2017-06-09 20:12:15 +0300

    Add data to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 5.times do |n|
   name  = "user#{n+1}"
   email = "user#{n+1}@gmail.com"
-  password  = "password"
+  password  = 'password'
   User.create_with(name: name,
                    password: password).find_or_create_by(email: email)
 end
@@ -9,3 +9,9 @@ end
 User.create_with(name: 'admin', password: 'password')
     .find_or_create_by(email:'admin@gmail.com')
     .add_role(:admin)
+
+User.last(5).each do |u|
+  3.times do |n|
+    User.first.send_message(u, "Text#{n}", "subject#{n}")
+  end
+end
